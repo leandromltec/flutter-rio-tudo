7d391f568a07444601ebf404981434831425748a
Leandro Loureiro <leandroloureiro@MacBook-Air-de-Leandro.local>
2023-07-31 18:03:46 -0300

    add test

---
+++ packages/rio_tudo/test/presentation/pages/influencers_screen_test.dart
@@ -0,0 +1,20 @@
+import 'package:config/config.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rio_tudo/src/presentation/pages/pages.dart';
+
+void main() {
+  Future<void> loadPage(WidgetTester tester) async {
+    Widget page = const MaterialApp(
+      home: InfluencersScreen(),
+    );
+
+    await tester.pumpWidget(page, const Duration(seconds: 1));
+  }
+
+  testWidgets('description', (widgetTester) async {
+    await loadPage(widgetTester);
+
+    expect(find.text(LabelsApp.influencersText), findsOneWidget);
+  });
+}
