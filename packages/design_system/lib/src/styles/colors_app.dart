6645f539de409dec962b4234014b364ff47caae9
Leandro Loureiro <leandroloureiro@MacBook-Air-de-Leandro.local>
2023-07-31 15:13:02 -0300

    design system

---
+++ packages/design_system/lib/src/styles/colors_app.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class DesignSystemColorApp extends Color {
+  const DesignSystemColorApp(int value) : super(value);
+}
+
+class DesignSystemPaletterColorApp {
+  DesignSystemPaletterColorApp._();
+
+//Main colors
+  static const primaryColor = DesignSystemColorApp(0Xff111111);
+  static const secondaryColor = DesignSystemColorApp(0Xffff7d00);
+  static const secondaryColorWhite = DesignSystemColorApp(0Xffffffff);
+
+  //Menu Colors
+  static const shadowItemMenu = DesignSystemColorApp(0Xff6c757d);
+
+  //Font Colors
+  static const fontColorDefault = Colors.black;
+  static const fontColorSecondary = DesignSystemColorApp(0Xffffffff);
+  static const fontColorHintText = DesignSystemColorApp(0Xff6c757d);
+  static const fontColorButton = DesignSystemColorApp(0Xfffaa307);
+  static const fontPrimaryColor = DesignSystemColorApp(0Xffffffff);
+
+  //Card Colors
+  static const cardColor = DesignSystemColorApp(0Xffe85d04);
+  static const cardColorSubtitle = DesignSystemColorApp(0Xfffaa307);
+  static const cardColorButtonLink = DesignSystemColorApp(0Xff8c34be);
+  static const cardColorNoSubtitle = DesignSystemColorApp(0Xfffb8500);
+  static const cardColorButtonLinkNoSubtitle = DesignSystemColorApp(0Xffdb2266);
+  static const fontCardColorSubtitle = DesignSystemColorApp(0Xfff8f9fa);
+}
---
+++ packages/design_system/lib/src/styles/text_style_app.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'colors_app.dart';
+
+class DesignSystemTextStyleApp {
+  DesignSystemTextStyleApp._();
+
+  static String defaultFont = 'Roboto';
+
+  static TextStyle textTopPage = const TextStyle(
+      color: DesignSystemPaletterColorApp.secondaryColor, fontSize: 16);
+
+  static TextStyle titleCategory = const TextStyle(
+      fontSize: 14,
+      color: DesignSystemPaletterColorApp.secondaryColorWhite,
+      fontWeight: FontWeight.bold);
+
+  static TextStyle labelBottomNavigator = const TextStyle(
+      fontSize: 12,
+      color: DesignSystemPaletterColorApp.secondaryColorWhite,
+      fontWeight: FontWeight.bold);
+
+  static TextStyle nameCategory = const TextStyle(
+      fontSize: 12,
+      color: DesignSystemPaletterColorApp.secondaryColorWhite,
+      fontWeight: FontWeight.bold);
+
+  static TextStyle textButton =
+      const TextStyle(color: DesignSystemPaletterColorApp.fontColorButton);
+
+  static TextStyle titleTipCard = const TextStyle(
+      fontSize: 16, color: DesignSystemPaletterColorApp.fontPrimaryColor);
+
+  static TextStyle subTitleTipCard = const TextStyle(
+      color: DesignSystemPaletterColorApp.fontPrimaryColor,
+      fontSize: 12,
+      fontWeight: FontWeight.bold);
+
+  static TextStyle textFavoriteScreen = const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      color: DesignSystemPaletterColorApp.secondaryColor);
+
+  static TextStyle titleInfoScreen = const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      color: DesignSystemPaletterColorApp.secondaryColor);
+
+  static TextStyle textInfoScreen = const TextStyle(
+      fontSize: 14, color: DesignSystemPaletterColorApp.primaryColor);
+}
